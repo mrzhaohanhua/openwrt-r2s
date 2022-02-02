2ac45ce1a302ea8dfe4ac285a8a6cf3540193c1b
Zhao Hanhua <40707495+mrzhaohanhua@users.noreply.github.com>
2022-02-02 08:28:25 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,14 @@ git clone --depth 1 -b v21.02.1 https://github.com/openwrt/openwrt openwrt
 #切换到openwrt目录
 cd openwrt
 
+# 配置 IRQ 并默认关闭 eth0 offloading rx/rx
+sed -i '/set_interface_core 4 "eth1"/a\\tset_interface_core 1 "ff150000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
+sed -i '/ff150000.i2c/a\\tset_interface_core 8 "ff160000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
+wget -P target/linux/rockchip/armv8/base-files/etc/hotplug.d/iface/ https://github.com/QiuSimons/OpenWrt-Add/raw/master/12-disable-rk3328-eth-offloading
+# 交换 LAN/WAN 口
+sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+
 # 更新 Feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
